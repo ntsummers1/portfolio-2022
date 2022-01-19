7a025ed5266c3dfad40376e4c36bd435e1238ff6
ntsummers1 <ntsummers1@users.noreply.github.com>
2022-01-19 03:46:34 +0000

    deploy: 9a16f602cb953d586d9175a3c716fb33f75921e1

---
+++ _next/static/gDx6m5yrXTp_GBv_bcUFd/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/305f3c97e111352f.css","static/chunks/pages/index-604308b4378f9eb0.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
