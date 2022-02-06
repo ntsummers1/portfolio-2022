bbadc6fbccea54ab999a99d524dce0c8308ba7dc
Nicholas Summers <ntsummers1@gmail.com>
2022-02-05 21:41:21 -0500

    Real rough learning pass at fancy scroll elements

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,54 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
+import { useInView } from "react-intersection-observer";
 import styles from "../styles/Home.module.scss";
 
 const Home: NextPage = () => {
+  const outsideImageRef = useRef(null);
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const [scale, setScale] = useState(1)
+  useEffect(() => {
+    function onScroll() {
+      let currentPosition = window.pageYOffset; // or use document.documentElement.scrollTop;
+      if (currentPosition > 0 && currentPosition < 920) {
+        if (scrollPosition < currentPosition) {
+          //User is scrolling down, therefor the image should be zooming out to reveal outside image
+          console.log('down')
+          if (scale < 0.5) {
+            setScale(0.5)
+          } else if (scale  > 0.5) {
+            setScale(scale - currentPosition * 0.00011)
+          }
+          console.log(`scale: ${scale}`)
+        } else if (scrollPosition > currentPosition) {
+          console.log('up')
+          //User is scrolling up, therefor the image should be zooming in on inside image
+          if (scale > 1) {
+            setScale(1)
+          } else if (scale < 1){
+            setScale(scale + currentPosition * 0.00011)
+          }
+          console.log(`scale: ${scale}`)
+        }
+      }
+      if (currentPosition == 0) {
+        console.log(`currentPosition == 0 ${currentPosition}`)
+        setScale(1)
+      }
+      setScrollPosition(currentPosition <= 0 ? 0 : currentPosition);
+      outsideImageRef.current.style = `transform: translate3d(0px, 0px, 0px) scale3d(${scale}, ${scale}, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg);`;
+    }
+
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [scrollPosition, scale]);
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Nicholas Summers - Resume</title>
+        <title>Nicholas Summers Portfolio</title>
         <meta
           name="description"
           content="Nicholas Summers' Portfolio Website"
@@ -16,296 +57,81 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1>Resume</h1>
-        <div className={[styles.row, styles.rowLinks].join(" ")}>
-          <div className={styles.columnThird}>
-            <p>Email</p>
-            <a href="mailto:ntsummers1@gmail.com?subject = Website&body = Message">
-              ntsummers1@gmail.com
-            </a>
+        <div className={styles.scrollingHeroWrap}>
+          <div className={styles.scrollingHeroTrigger}></div>
+          <div className={styles.scrollingHeroStickyWrap}>
+            <div className={styles.scrollingHeroOverflowWrap}>
+              <header
+                className={styles.scrollingHero}
+                ref={outsideImageRef}
+                style={{
+                  transform:
+                    "translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)",
+                }}
+              >
+                <img
+                  src={"/images/outside-image.png"}
+                  alt="outside-image"
+                  className={styles.heroImage}
+                ></img>
+                <div className={styles.scrollingHeroInsideContainer}>
+                  <div className={styles.scrollingHeroInsideImageWrap}>
+                    <div className={styles.scrollingHeroInsideImageContainer}>
+                      <img
+                        src={"/images/inside-image.jpeg"}
+                        alt="inside-image"
+                        className={styles.heroImageInside}
+                      ></img>
+                    </div>
+                  </div>
+                </div>
+              </header>
+            </div>
           </div>
-          <div className={styles.columnThird}>
-            <p>Linkedin</p>
-            <a href="https://www.linkedin.com/in/nicholas-summers-theo/">
-              linkedin.com/in/nicholas-summers-theo
-            </a>
-          </div>
-          <div className={styles.columnThird}>
-            <p>GitHub</p>
-            <a href="https://github.com/ntsummers1">github.com/ntsummers1</a>
-          </div>
+          <div className={styles.scrollingHeroStickyGap}></div>
         </div>
-
-        <div className={styles.row}>
-          <div className={styles.columnMain}>
-            <div className={styles.section}>
-              <h2> Preface</h2>
-              <p>
-                This resume tells the story of me and how I came into coding and
-                development. This is so you can gain an understanding of how I
-                got to where I am today. Think of it as looking at your
-                instagram feed - reading oldest to newest. I also have your
-                standard professional resume here should you prefer that format.
-              </p>
-              <button> Download Professional Resume </button>
-            </div>
-
-            <div className={[styles.section, styles.education].join(" ")}>
-              <h2>Education</h2>
-
-              <article>
-                <h3>Michigan State University</h3>
-                <ul>
-                  <li>Degree: Bachelor of Science</li>
-                  <li>Study: Computer Science</li>
-                </ul>
-                <p>
-                  Spicy jalapeno bacon ipsum dolor amet meatloaf tri-tip
-                  chislic, shoulder est chuck pork belly aute jowl shankle
-                  cillum. Pig tongue kielbasa porchetta commodo chicken cupim
-                  proident. Strip steak pork chop irure incididunt adipisicing
-                  tongue elit sed quis pig buffalo eu burgdoggen swine veniam.
-                  Labore shank flank esse burgdoggen corned beef.
-                </p>
-              </article>
-
-              <article>
-                <h3>Michigan State University</h3>
-                <ul>
-                  <li>Degree: Bachelor of Science</li>
-                  <li>Study: Media and Information</li>
-                </ul>
-                <p>
-                  Spicy jalapeno bacon ipsum dolor amet meatloaf tri-tip
-                  chislic, shoulder est chuck pork belly aute jowl shankle
-                  cillum. Pig tongue kielbasa porchetta commodo chicken cupim
-                  proident. Strip steak pork chop irure incididunt adipisicing
-                  tongue elit sed quis pig buffalo eu burgdoggen swine veniam.
-                  Labore shank flank esse burgdoggen corned beef.
-                </p>
-              </article>
-            </div>
-
-            <div className={[styles.section, styles.experience].join(" ")}>
-              <h2> Experience </h2>
-              <article>
-                <h3>Student Web Developer</h3>
-                <ul>
-                  <li>Company: Michigan State University</li>
-                  <li>Timeframe: 2019 - Now</li>
-                </ul>
-                <p>
-                  Spicy jalapeno bacon ipsum dolor amet meatloaf tri-tip
-                  chislic, shoulder est chuck pork belly aute jowl shankle
-                  cillum. Pig tongue kielbasa porchetta commodo chicken cupim
-                  proident. Strip steak pork chop irure incididunt adipisicing
-                  tongue elit sed quis pig buffalo eu burgdoggen swine veniam.
-                  Labore shank flank esse burgdoggen corned beef.
-                </p>
-              </article>
-
-              <article>
-                <h3>IT Intern</h3>
-                <ul>
-                  <li>Company: Kellogs</li>
-                  <li>Timeframe: Apr. 2015 - Oct. 2015</li>
-                </ul>
-                <p>
-                  Spicy jalapeno bacon ipsum dolor amet meatloaf tri-tip
-                  chislic, shoulder est chuck pork belly aute jowl shankle
-                  cillum. Pig tongue kielbasa porchetta commodo chicken cupim
-                  proident. Strip steak pork chop irure incididunt adipisicing
-                  tongue elit sed quis pig buffalo eu burgdoggen swine veniam.
-                  Labore shank flank esse burgdoggen corned beef.
-                </p>
-              </article>
-
-              <article>
-                <h3>Mobile Developer/Designer</h3>
-                <ul>
-                  <li>Company: Michigan State University</li>
-                  <li>Timeframe: 2019 - Now</li>
-                </ul>
-                <p>
-                  Spicy jalapeno bacon ipsum dolor amet meatloaf tri-tip
-                  chislic, shoulder est chuck pork belly aute jowl shankle
-                  cillum. Pig tongue kielbasa porchetta commodo chicken cupim
-                  proident. Strip steak pork chop irure incididunt adipisicing
-                  tongue elit sed quis pig buffalo eu burgdoggen swine veniam.
-                  Labore shank flank esse burgdoggen corned beef.
-                </p>
-              </article>
-
-              <article>
-                <h3>Application Developer III</h3>
-                <ul>
-                  <li>Company: Rutgers University</li>
-                  <li>Timeframe: 2019 - Now</li>
-                </ul>
-                <p>
-                  Spicy jalapeno bacon ipsum dolor amet meatloaf tri-tip
-                  chislic, shoulder est chuck pork belly aute jowl shankle
-                  cillum. Pig tongue kielbasa porchetta commodo chicken cupim
-                  proident. Strip steak pork chop irure incididunt adipisicing
-                  tongue elit sed quis pig buffalo eu burgdoggen swine veniam.
-                  Labore shank flank esse burgdoggen corned beef.
-                </p>
-              </article>
-            </div>
-          </div>
-
-          <div className={styles.columnSub}>
-            <div className={[styles.section, styles.skills].join(" ")}>
-              <h2>Expectional Skills</h2>
-
-              <article>
-                <h4>Human-Oriented Design</h4>
-                <p>
-                  Spicy jalapeno bacon ipsum dolor amet meatloaf tri-tip
-                  chislic, shoulder est chuck pork belly aute jowl shankle
-                  cillum. Pig tongue kielbasa porchetta commodo chicken cupim
-                  proident.
-                </p>
-              </article>
-
-              <article>
-                <h4>Skill 2</h4>
-                <p>
-                  Spicy jalapeno bacon ipsum dolor amet meatloaf tri-tip
-                  chislic, shoulder est chuck pork belly aute jowl shankle
-                  cillum. Pig tongue kielbasa porchetta commodo chicken cupim
-                  proident.
-                </p>
-              </article>
-
-              <article>
-                <h4>Skill 3</h4>
-                <p>
-                  Spicy jalapeno bacon ipsum dolor amet meatloaf tri-tip
-                  chislic, shoulder est chuck pork belly aute jowl shankle
-                  cillum. Pig tongue kielbasa porchetta commodo chicken cupim
-                  proident.
-                </p>
-              </article>
-            </div>
-
-            <div className={styles.section}>
-              <h2>Tools/Code of the Trade</h2>
-
-              <article className={styles.tools}>
-                <div className={styles.tool}>
-                  <p> Android </p>
-                </div>
-                <div className={styles.tool}>
-                  <p> Angular </p>
-                </div>
-                <div className={styles.tool}>
-                  <p> Babel </p>
-                </div>
-                <div className={styles.tool}>
-                  <p> Bootstrap </p>
-                </div>
-
-                <div className={styles.tool}>
-                  <p> CSS3 </p>
-                </div>
-                <div className={styles.tool}>
-                  <p> Dart </p>
-                </div>
-                <div className={styles.tool}>
-                  <p> Express </p>
-                </div>
-                <div className={styles.tool}>
-                  <p> Figma </p>
-                </div>
-                <div className={styles.tool}>
-                  <p> Firebase </p>
-                </div>
-                <div className={styles.tool}>
-                  <p> Flutter </p>
-                </div>
-                <div className={styles.tool}>
-                  <p> GraphQL </p>
-                </div>
-                <div className={styles.tool}>
-                  <p> Gulp </p>
-                </div>
-                <div className={styles.tool}>
-                  <p> HTML5 </p>
-                </div>
-                <div className={styles.tool}>
-                  <p> Illustrator </p>
-                </div>
-                <div className={styles.tool}>
-                  <p> Invision </p>
-                </div>
-                <div className={styles.tool}>
-                  <p> Java </p>
-                </div>
-                <div className={styles.tool}>
-                  <p> Javascript </p>
-                </div>
-                <div className={styles.tool}>
-                  <p> Kotlin </p>
-                </div>
-                <div className={styles.tool}>
-                  <p> Materialize </p>
-                </div>
-                <div className={styles.tool}>
-                  <p> MySQL </p>
-                </div>
-                <div className={styles.tool}>
-                  <p> NextJS </p>
-                </div>
-                <div className={styles.tool}>
-                  <p> NodeJS </p>
-                </div>
-                <div className={styles.tool}>
-                  <p> Photoshop </p>
-                </div>
-                <div className={styles.tool}>
-                  <p> PHP </p>
-                </div>
-                <div className={styles.tool}>
-                  <p> Postman </p>
-                </div>
-                <div className={styles.tool}>
-                  <p> ReactJS </p>
-                </div>
-                <div className={styles.tool}>
-                  <p> React Native </p>
-                </div>
-                <div className={styles.tool}>
-                  <p> Redux </p>
-                </div>
-                <div className={styles.tool}>
-                  <p> Sass </p>
-                </div>
-                <div className={styles.tool}>
-                  <p> Sketch </p>
-                </div>
-                <div className={styles.tool}>
-                  <p> Spring </p>
-                </div>
-                <div className={styles.tool}>
-                  <p> Tailwind </p>
-                </div>
-                <div className={styles.tool}>
-                  <p> TypeScript </p>
-                </div>
-                <div className={styles.tool}>
-                  <p> VueJS </p>
-                </div>
-                <div className={styles.tool}>
-                  <p> Vuetify </p>
-                </div>
-                <div className={styles.tool}>
-                  <p> Webpack </p>
-                </div>
-              </article>
-            </div>
-          </div>
-        </div>
+        <section>
+          <p style={{height: '400vh'}}>
+            Bacon ipsum dolor amet short ribs tri-tip pancetta frankfurter
+            jerky, ham salami bacon swine fatback pork belly. Short ribs
+            hamburger buffalo, ribeye pancetta drumstick burgdoggen beef ribs.
+            Pork loin ball tip cupim, hamburger tail leberkas spare ribs
+            prosciutto landjaeger pastrami corned beef pancetta sirloin fatback.
+            Jerky jowl frankfurter landjaeger boudin beef meatloaf short loin
+            picanha. Cow tail doner hamburger swine pancetta pastrami. Pork
+            belly sausage pastrami, strip steak turducken boudin brisket jowl
+            kielbasa ribeye capicola pig hamburger fatback shankle. Porchetta
+            andouille buffalo frankfurter leberkas bacon jowl corned beef
+            picanha biltong sausage kielbasa alcatra boudin salami. Landjaeger
+            biltong ham hock, tenderloin pork loin porchetta pork belly ball tip
+            prosciutto flank cupim short loin pancetta salami short ribs. Short
+            ribs capicola ham, short loin chuck beef brisket. Rump pork loin
+            flank swine chicken meatball leberkas jerky ball tip hamburger
+            ribeye porchetta buffalo ground round shoulder. Leberkas landjaeger
+            turkey, boudin bresaola ham hock fatback strip steak beef. Pastrami
+            turducken jerky brisket filet mignon prosciutto biltong shank. Tail
+            meatball ham burgdoggen pancetta biltong. Chislic hamburger turkey,
+            beef ribs t-bone alcatra ham hock shank shankle sirloin. T-bone pork
+            loin andouille capicola brisket pancetta. Capicola prosciutto
+            brisket venison chicken bresaola chuck pork loin. Hamburger brisket
+            t-bone beef drumstick pork doner pork loin frankfurter chislic
+            flank. Pork loin ham sausage picanha. Leberkas meatloaf pork beef
+            ribs landjaeger sausage chicken tenderloin tri-tip biltong. Doner
+            cow shank, salami pork loin rump tri-tip filet mignon ham hock
+            turkey pork. Bacon jerky salami pancetta, kevin turducken tail
+            turkey ball tip sirloin sausage. Porchetta hamburger pork flank
+            andouille. Filet mignon tri-tip andouille t-bone, jowl alcatra flank
+            burgdoggen meatball sausage ham pork belly ham hock meatloaf. Short
+            loin filet mignon tongue, cow biltong capicola cupim salami
+            turducken strip steak pork chop doner pork loin bacon. Ground round
+            leberkas doner turkey, meatball cupim brisket flank porchetta
+            meatloaf. Frankfurter ground round meatloaf, turkey bacon cupim
+            rump. Bacon pork corned beef, pork belly ham prosciutto venison
+            andouille meatball. Ham hock pig buffalo meatloaf picanha filet
+            mignon ball tip spare ribs. Chicken turkey meatball hamburger swine
+            tri-tip pork loin chuck filet mignon turducken buffalo salami.
+          </p>
+        </section>
       </main>
 
       <footer className={styles.footer}></footer>
