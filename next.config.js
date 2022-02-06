b2ab2ea51ee57ff98aa2ad35d03c35875631f28e
Nicholas Summers <ntsummers1@gmail.com>
2022-02-06 17:15:00 -0500

    Fix up next.config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
 const path = require('path')
 
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
+  assetPrefix: isProd ? '/portfolio-2022/' : '',
   reactStrictMode: true,
+  images: {
+    domains: [
+      'raw.githubusercontent.com',
+      'angular.io',
+      'www.vectorlogo.zone',
+      'cdn.worldvectorlogo.com',
+      'reactnative.dev',
+      'bestofjs.org',
+    ],
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
